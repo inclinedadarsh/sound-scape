00de4297742a2a3e65a7046e3adea57a10da7b78
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-06-15 22:21:08 +0530

    feat: :art: install and setup prettier with some custom rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,24 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: ["airbnb", "airbnb/hooks"],
+	extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
 	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
+		ecmaVersion: 'latest',
+		sourceType: 'module',
 		ecmaFeatures: {
 			arrowFunctions: false,
 		},
 	},
+	plugins: ['prettier'],
 	rules: {
-		"react/react-in-jsx-scope": "off",
-		indent: ["error", "tab"],
-		"no-tabs": "off",
-		"react/jsx-indent": ["error", "tab"],
-		"react/function-component-definition": [
-			"error",
+		'react/react-in-jsx-scope': 'off',
+		indent: ['error', 'tab'],
+		'no-tabs': 'off',
+		'react/jsx-indent': ['error', 'tab'],
+		'react/function-component-definition': [
+			'error',
 			{
-				namedComponents: "arrow-function",
+				namedComponents: 'arrow-function',
 			},
 		],
 	},
